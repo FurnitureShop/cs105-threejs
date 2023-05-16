2c92f6119aa6aa2f8c8c22ab6e4c7bbe892843ac
PhamneyuT <koduapass@gmail.com>
2023-05-16 23:13:08 +0700

    setup base code

---
+++ src/Experience/Camera.ts
@@ -0,0 +1,64 @@
+import * as THREE from "three";
+import Experience from "./Experience";
+
+export default class Camera {
+    experience: Experience;
+    sizes: any;
+    scene: any;
+    canvas: any;
+
+    perspectiveCamera!: THREE.PerspectiveCamera;
+    orthographicCamera!: THREE.OrthographicCamera;
+    frustrum!: number;
+
+    constructor() {
+        this.experience = new Experience();
+        this.sizes = this.experience.sizes;
+        this.scene = this.experience.scene;
+        this.canvas = this.experience.canvas;
+        // console.log(this.experience, this.sizes, this.scene, this.canvas)
+        this.createPerspectiveCamera();
+        this.createOrthographicCamera();
+    }
+
+    createPerspectiveCamera() {
+        this.perspectiveCamera = new THREE.PerspectiveCamera(
+            35,
+            this.sizes.aspect,
+            0.1,
+            1000
+        )
+        this.scene.add(this.perspectiveCamera)
+        this.perspectiveCamera.position.z = 5
+    }
+
+    createOrthographicCamera() {
+        this.frustrum = 5
+        this.orthographicCamera = new THREE.OrthographicCamera(
+            (-this.sizes.aspect * this.frustrum) / 2,
+            (this.sizes.aspect * this.frustrum) / 2,
+            this.frustrum / 2,
+            -this.frustrum / 2,
+            -100,
+            100
+        )
+        this.scene.add(this.orthographicCamera)
+    }
+
+    resize() {
+        //Update Perspective Camera on resize
+        this.perspectiveCamera.aspect = this.sizes.aspect
+        this.perspectiveCamera.updateProjectionMatrix();
+
+        //Update Orthographic Camera on resize
+        this.orthographicCamera.left = (-this.sizes.aspect * this.frustrum) / 2
+        this.orthographicCamera.right = (this.sizes.aspect * this.frustrum) / 2
+        this.orthographicCamera.top = this.frustrum / 2
+        this.orthographicCamera.bottom = -this.frustrum / 2
+        this.orthographicCamera.updateProjectionMatrix()
+    }
+
+    update() {
+
+    }
+}
